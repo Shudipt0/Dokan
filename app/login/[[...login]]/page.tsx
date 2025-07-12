2f5b0c141ae3aa45f04f861c2547450afb8269bb
Shudipt0 <royshudipto318@gmail.com>
2025-07-13 00:32:46 +0600

    auth form setup complete

--- app/login/[[...login]]/page.tsx
+++ app/login/[[...login]]/page.tsx
@@ -1,15 +1,42 @@
-import { SignIn } from '@clerk/nextjs'
+"use client";
 
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+// import { SignIn } from '@clerk/nextjs'
 
-const page = () => {
+const LogInPage = () => {
   return (
-    <div className='flex flex-col h-screen'>
-        <div className='m-auto'>
-            <SignIn forceRedirectUrl='/'/>
-        </div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col h-screen">
+      <div className="m-auto">
+        {/* <SignIn forceRedirectUrl='/'/> */}
 
-export default page
+        <SignIn.Root>
+          <SignIn.Step name="start">
+            <h1>Sign in to your account</h1>
+
+            <Clerk.Connection name="google">
+              Sign in with Google
+            </Clerk.Connection>
+
+            <Clerk.Field name="identifier">
+              <Clerk.Label>Email</Clerk.Label>
+              <Clerk.Input />
+              <Clerk.FieldError />
+            </Clerk.Field>
+
+            <h2>Enter your password</h2>
+
+            <Clerk.Field name="password">
+              <Clerk.Label>Password</Clerk.Label>
+              <Clerk.Input type="password" />
+              <Clerk.FieldError />
+            </Clerk.Field>
+            <SignIn.Action submit>Log In</SignIn.Action>
+          </SignIn.Step>
+        </SignIn.Root>
+      </div>
+    </div>
+  );
+};
+
+export default LogInPage;
--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -1,85 +1,67 @@
-'use client'
+"use client";
 
-import * as Clerk from '@clerk/elements/common'
-import * as SignUp from '@clerk/elements/sign-up'
+import * as Clerk from "@clerk/elements/common";
+import * as SignUp from "@clerk/elements/sign-up";
 
 // import { SignUp } from '@clerk/nextjs'
-import {
-  useSignUp,
-  useClerk,
-  
-} from '@clerk/nextjs';
-import { useRouter } from "next/navigation";
-
 
 const SignUpPage = () => {
-    const router = useRouter();
-  const { signUp } = useSignUp();
-  const { setActive } = useClerk();
-
-  const handleComplete = async () => {
-    if (signUp?.status === 'complete') {
-      await setActive({ session: signUp.createdSessionId });
-      router.push('/');
-    }
-  };
   return (
-    <div className='flex flex-col h-screen'>
-        <div className='m-auto'>
-            {/* <SignUp forceRedirectUrl='/' /> */}
+    <div className="flex flex-col h-screen">
+      <div className="m-auto">
+        {/* <SignUp forceRedirectUrl='/' /> */}
 
-                <SignUp.Root  >
-      <SignUp.Step name="start" 
-      // Redirect after sign-up is complete
-         >
-        <h1>Create an account</h1>
+        <SignUp.Root>
+          <SignUp.Step
+            name="start"
+            // Redirect after sign-up is complete
+          >
+            <h1>Create an account</h1>
 
-        <Clerk.Connection name="google">Sign up with Google</Clerk.Connection>
+            <Clerk.Connection name="google">
+              Sign up with Google
+            </Clerk.Connection>
 
-        <Clerk.Field name="username">
-          <Clerk.Label>Username</Clerk.Label>
-          <Clerk.Input />
-          <Clerk.FieldError />
-        </Clerk.Field>
+            <Clerk.Field name="username">
+              <Clerk.Label>Username</Clerk.Label>
+              <Clerk.Input />
+              <Clerk.FieldError />
+            </Clerk.Field>
 
-        <Clerk.Field name="emailAddress">
-          <Clerk.Label>Email</Clerk.Label>
-          <Clerk.Input />
-          <Clerk.FieldError />
-        </Clerk.Field>
+            <Clerk.Field name="emailAddress">
+              <Clerk.Label>Email</Clerk.Label>
+              <Clerk.Input />
+              <Clerk.FieldError />
+            </Clerk.Field>
 
-        <Clerk.Field name="password">
-          <Clerk.Label>Password</Clerk.Label>
-          <Clerk.Input />
-          <Clerk.FieldError />
-        </Clerk.Field>
+            <Clerk.Field name="password">
+              <Clerk.Label>Password</Clerk.Label>
+              <Clerk.Input />
+              <Clerk.FieldError />
+            </Clerk.Field>
 
-        <SignUp.Action submit onComplete={handleComplete}>Sign up</SignUp.Action>
-      </SignUp.Step>
+            <SignUp.Action submit>Sign up</SignUp.Action>
+          </SignUp.Step>
 
+          <SignUp.Step name="verifications">
+            <SignUp.Strategy name="email_code">
+              <h1>Check your email</h1>
 
-      <SignUp.Step name="verifications">
-        
+              <Clerk.Field name="code">
+                <Clerk.Label>Email Code</Clerk.Label>
+                <Clerk.Input />
+                <Clerk.FieldError />
+              </Clerk.Field>
 
-        <SignUp.Strategy name="email_code">
-          <h1>Check your email</h1>
-
-          <Clerk.Field name="code">
-            <Clerk.Label>Email Code</Clerk.Label>
-            <Clerk.Input />
-            <Clerk.FieldError />
-          </Clerk.Field>
-
-          <SignUp.Action submit >Verify</SignUp.Action>
-        </SignUp.Strategy>
-      </SignUp.Step>
-      {/* CAPTCHA Widget */}
+              <SignUp.Action submit>Verify</SignUp.Action>
+            </SignUp.Strategy>
+          </SignUp.Step>
+          {/* CAPTCHA Widget */}
           <div id="clerk-captcha"></div>
-    </SignUp.Root>
-        </div>
-      
+        </SignUp.Root>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
