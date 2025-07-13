77663b9d9d9b40fb438abd6815d11d6f7b58ebec
Shudipt0 <royshudipto318@gmail.com>
2025-07-13 18:21:48 +0600

    working in forget password

--- app/login/[[...login]]/page.tsx
+++ app/login/[[...login]]/page.tsx
@@ -2,38 +2,90 @@
 
 import * as Clerk from "@clerk/elements/common";
 import * as SignIn from "@clerk/elements/sign-in";
-// import { SignIn } from '@clerk/nextjs'
+import Image from "next/image";
+import sideImage from "@/public/SideImage.jpg";
+import { FcGoogle } from "react-icons/fc";
+import Link from "next/link";
 
 const LogInPage = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <div className="m-auto">
-        {/* <SignIn forceRedirectUrl='/'/> */}
+    <div className="container mx-auto px-2 md:px-14 md:py-20 bg-white">
+      <div className="w-full md:flex items-center justify-between">
+        <div>
+          <Image src={sideImage} alt="sideimage" width={800} height={780} />
+        </div>
+        <div className="md:w-[370px] md:h-[530px]  ">
+          <SignIn.Root>
+            <SignIn.Step
+              name="start"
+              className="w-full h-full flex flex-col justify-between "
+            >
+              <div className="space-y-4">
+                <h1 className="text-4xl text-black/90 font-semibold">
+                  Log in to Exclusive
+                </h1>
+                <p className="text-md text-black/80 font-semibold">
+                  Enter your details below
+                </p>
+              </div>
 
-        <SignIn.Root>
-          <SignIn.Step name="start">
-            <h1>Sign in to your account</h1>
+              <Clerk.Field name="identifier" className="w-full ">
+                <Clerk.Label className="flex items-center relative">
+                  <Clerk.Input className="outline-none peer text-[18px] text-black/80 font-semibold duration-200 " />
+                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200  peer-valid:text-sm peer-valid:bg-white">
+                    Email
+                  </span>
+                </Clerk.Label>
+                <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
+                <Clerk.FieldError className="text-sm text-red-500 " />
+              </Clerk.Field>
 
-            <Clerk.Connection name="google">
-              Sign in with Google
-            </Clerk.Connection>
+              <Clerk.Field name="password" className="w-full ">
+                <Clerk.Label className="flex items-center relative">
+                  <Clerk.Input
+                    type="password"
+                    className="outline-none peer text-[18px] text-black/80 font-semibold duration-200 "
+                  />
+                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200  peer-valid:text-sm peer-valid:bg-white">
+                    Password
+                  </span>
+                </Clerk.Label>
+                <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
+                <Clerk.FieldError className="text-sm text-red-500 " />
+              </Clerk.Field>
+              <div className="space-y-5">
+              <div className="flex items-center justify-between">
+                <SignIn.Action
+                  submit
+                  className="w-[145px] h-14 flex items-center justify-center text-md text-white bg-[#DB4444] hover:bg-[#DB5555] cursor-pointer rounded "
+                >
+                  Log In
+                </SignIn.Action>
+                <h3 className="text-md text-[#DB4444]">Forget Password?</h3>
+              </div>
+              <Clerk.Connection
+                name="google"
+                className="w-full h-14 flex items-center justify-center gap-3 border border-gray-700 text-md text-black/90 cursor-pointer rounded "
+              >
+                <span>
+                  <FcGoogle size={24} />
+                </span>
+                Log in with Google
+              </Clerk.Connection>
+              </div>
 
-            <Clerk.Field name="identifier">
-              <Clerk.Label>Email</Clerk.Label>
-              <Clerk.Input />
-              <Clerk.FieldError />
-            </Clerk.Field>
-
-            <h2>Enter your password</h2>
-
-            <Clerk.Field name="password">
-              <Clerk.Label>Password</Clerk.Label>
-              <Clerk.Input type="password" />
-              <Clerk.FieldError />
-            </Clerk.Field>
-            <SignIn.Action submit>Log In</SignIn.Action>
-          </SignIn.Step>
-        </SignIn.Root>
+              <div className="w-full flex items-center justify-center gap-3">
+                <p className="text-md text-gray-600 ">Didn't have account?</p>
+                <Link
+                  href="login"
+                  className="text-md text-gray-700 underline underline-gray-400 underline-offset-6"
+                >
+                  Sign in
+                </Link>
+              </div>
+            </SignIn.Step>
+          </SignIn.Root>
+        </div>
       </div>
     </div>
   );
--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -2,63 +2,113 @@
 
 import * as Clerk from "@clerk/elements/common";
 import * as SignUp from "@clerk/elements/sign-up";
-
-// import { SignUp } from '@clerk/nextjs'
+import Image from "next/image";
+import sideImage from "@/public/SideImage.jpg";
+import { FcGoogle } from "react-icons/fc";
+import Link from "next/link";
 
 const SignUpPage = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <div className="m-auto">
-        {/* <SignUp forceRedirectUrl='/' /> */}
+    <div className="container mx-auto px-2 md:px-14 md:py-20 bg-white">
+      <div className="w-full md:flex items-center justify-between">
+        <div>
+          <Image src={sideImage} alt="sideimage" width={800} height={780} />
+        </div>
 
-        <SignUp.Root>
-          <SignUp.Step
-            name="start"
-            // Redirect after sign-up is complete
-          >
-            <h1>Create an account</h1>
+        <div className="md:w-[370px] md:h-[530px]  ">
+          <SignUp.Root>
+            <SignUp.Step
+              name="start"
+              className="w-full h-full flex flex-col justify-between "
+            >
+              <div className="space-y-4">
+                <h1 className="text-4xl text-black/90 font-semibold">
+                  Create an account
+                </h1>
+                <p className="text-md text-black/80 font-semibold">
+                  Enter your details below
+                </p>
+              </div>
 
-            <Clerk.Connection name="google">
-              Sign up with Google
-            </Clerk.Connection>
-
-            <Clerk.Field name="username">
-              <Clerk.Label>Username</Clerk.Label>
-              <Clerk.Input />
-              <Clerk.FieldError />
-            </Clerk.Field>
-
-            <Clerk.Field name="emailAddress">
-              <Clerk.Label>Email</Clerk.Label>
-              <Clerk.Input />
-              <Clerk.FieldError />
-            </Clerk.Field>
-
-            <Clerk.Field name="password">
-              <Clerk.Label>Password</Clerk.Label>
-              <Clerk.Input />
-              <Clerk.FieldError />
-            </Clerk.Field>
-
-            <SignUp.Action submit>Sign up</SignUp.Action>
-          </SignUp.Step>
-
-          <SignUp.Step name="verifications">
-            <SignUp.Strategy name="email_code">
-              <h1>Check your email</h1>
-
-              <Clerk.Field name="code">
-                <Clerk.Label>Email Code</Clerk.Label>
-                <Clerk.Input />
-                <Clerk.FieldError />
+              <Clerk.Field name="username" className="w-full ">
+                <Clerk.Label className="flex items-center relative">
+                  <Clerk.Input className="outline-none peer text-[18px] text-black/80 font-semibold duration-200 focus:bg-white " />
+                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200  peer-valid:text-sm peer-valid:bg-white ">
+                    Name
+                  </span>
+                </Clerk.Label>
+                <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
+                <Clerk.FieldError className="text-sm text-red-500 " />
               </Clerk.Field>
 
-              <SignUp.Action submit>Verify</SignUp.Action>
-            </SignUp.Strategy>
-          </SignUp.Step>
-          {/* CAPTCHA Widget */}
-          <div id="clerk-captcha"></div>
-        </SignUp.Root>
+              <Clerk.Field name="emailAddress" className="w-full ">
+                <Clerk.Label className="flex items-center relative">
+                  <Clerk.Input className="outline-none peer text-[18px] text-black/80 font-semibold duration-200 " />
+                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200  peer-valid:text-sm peer-valid:bg-white">
+                    Email
+                  </span>
+                </Clerk.Label>
+                <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
+                <Clerk.FieldError className="text-sm text-red-500 "/>
+              </Clerk.Field>
+
+              <Clerk.Field name="password" className="w-full ">
+                <Clerk.Label className="flex items-center relative">
+                  <Clerk.Input className="outline-none peer text-[18px] text-black/80 font-semibold duration-200 " />
+                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200  peer-valid:text-sm peer-valid:bg-white">
+                    Password
+                  </span>
+                </Clerk.Label>
+                <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
+                <Clerk.FieldError className="text-sm text-red-500 "/>
+              </Clerk.Field>
+              <div className="space-y-5">
+                <SignUp.Action
+                  submit
+                  className="w-full h-14 flex items-center justify-center text-md text-white bg-[#DB4444] hover:bg-[#DB5555] cursor-pointer rounded "
+                >
+                  Create Account
+                </SignUp.Action>
+
+                <Clerk.Connection
+                  name="google"
+                  className="w-full h-14 flex items-center justify-center gap-3 border border-gray-700 text-md text-black/90 cursor-pointer rounded "
+                >
+                  <span>
+                    <FcGoogle size={24} />
+                  </span>
+                  Sign up with Google
+                </Clerk.Connection>
+              </div>
+
+              <div className="w-full flex items-center justify-center gap-3">
+                <p className="text-md text-gray-600 ">Already have account?</p>
+                <Link
+                  href="login"
+                  className="text-md text-gray-700 underline underline-gray-400 underline-offset-6"
+                >
+                  Log in
+                </Link>
+              </div>
+            </SignUp.Step>
+
+            <SignUp.Step name="verifications">
+              <SignUp.Strategy name="email_code">
+                <h1>Check your email</h1>
+
+                <Clerk.Field name="code">
+                  <Clerk.Label>Email Code</Clerk.Label>
+                  <Clerk.Input />
+                  <Clerk.FieldError />
+                </Clerk.Field>
+
+                <SignUp.Action submit>Verify</SignUp.Action>
+              </SignUp.Strategy>
+            </SignUp.Step>
+            {/* CAPTCHA Widget */}
+            <div id="clerk-captcha"></div>
+          </SignUp.Root>
+        </div>
       </div>
     </div>
   );
