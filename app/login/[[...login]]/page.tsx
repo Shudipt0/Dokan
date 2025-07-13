287f15bac3a5105031d6786c53d0105a736f8a40
Shudipt0 <royshudipto318@gmail.com>
2025-07-14 00:22:44 +0600

    working

--- app/login/[[...login]]/page.tsx
+++ app/login/[[...login]]/page.tsx
@@ -16,6 +16,7 @@ const LogInPage = () => {
         </div>
         <div className="md:w-[370px] md:h-[530px]  ">
           <SignIn.Root>
+            
             <SignIn.Step
               name="start"
               className="w-full h-full flex flex-col justify-between "
@@ -53,25 +54,32 @@ const LogInPage = () => {
                 <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
                 <Clerk.FieldError className="text-sm text-red-500 " />
               </Clerk.Field>
+
               <div className="space-y-5">
-              <div className="flex items-center justify-between">
-                <SignIn.Action
-                  submit
-                  className="w-[145px] h-14 flex items-center justify-center text-md text-white bg-[#DB4444] hover:bg-[#DB5555] cursor-pointer rounded "
+                <div className="flex items-center justify-between">
+                  <SignIn.Action
+                    submit
+                    className="w-[145px] h-14 flex items-center justify-center text-md text-white bg-[#DB4444] hover:bg-[#DB5555] cursor-pointer rounded "
+                  >
+                    Log In
+                  </SignIn.Action>
+
+                  <SignIn.Action
+                    navigate="forgot-password"
+                    className="text-md text-[#DB4444]"
+                  >
+                    Forgot password?
+                  </SignIn.Action>
+                </div>
+                <Clerk.Connection
+                  name="google"
+                  className="w-full h-14 flex items-center justify-center gap-3 border border-gray-700 text-md text-black/90 cursor-pointer rounded "
                 >
-                  Log In
-                </SignIn.Action>
-                <h3 className="text-md text-[#DB4444]">Forget Password?</h3>
-              </div>
-              <Clerk.Connection
-                name="google"
-                className="w-full h-14 flex items-center justify-center gap-3 border border-gray-700 text-md text-black/90 cursor-pointer rounded "
-              >
-                <span>
-                  <FcGoogle size={24} />
-                </span>
-                Log in with Google
-              </Clerk.Connection>
+                  <span>
+                    <FcGoogle size={24} />
+                  </span>
+                  Log in with Google
+                </Clerk.Connection>
               </div>
 
               <div className="w-full flex items-center justify-center gap-3">
@@ -84,6 +92,8 @@ const LogInPage = () => {
                 </Link>
               </div>
             </SignIn.Step>
+{/* forget pass */}
+ 
           </SignIn.Root>
         </div>
       </div>
--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -7,7 +7,7 @@ import sideImage from "@/public/SideImage.jpg";
 import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
 import { motion } from "motion/react";
-import { AnimatePresence } from "motion/react"
+import { AnimatePresence } from "motion/react";
 
 const SignUpPage = () => {
   return (
@@ -46,23 +46,23 @@ const SignUpPage = () => {
               <Clerk.Field name="emailAddress" className="w-full ">
                 <Clerk.Label className="flex items-center relative">
                   <Clerk.Input className="outline-none peer text-[18px] text-black/80 font-semibold duration-200 " />
-                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200  peer-valid:text-sm peer-valid:bg-white">
+                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200 peer-user-valid:-translate-y-5  peer-valid:text-sm peer-valid:bg-white">
                     Email
                   </span>
                 </Clerk.Label>
                 <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
-                <Clerk.FieldError className="text-sm text-red-500 "/>
+                <Clerk.FieldError className="text-sm text-red-500 " />
               </Clerk.Field>
 
               <Clerk.Field name="password" className="w-full ">
                 <Clerk.Label className="flex items-center relative">
                   <Clerk.Input className="outline-none peer text-[18px] text-black/80 font-semibold duration-200 " />
-                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200  peer-valid:text-sm peer-valid:bg-white">
+                  <span className="absolute left-0 text-[18px] text-gray-400 peer-focus:text-sm peer-focus:-translate-y-5 duration-200 peer-user-valid:-translate-y-5  peer-valid:text-sm peer-valid:bg-white">
                     Password
                   </span>
                 </Clerk.Label>
                 <div className="w-full h-[1.5px] my-1 bg-gray-300 "></div>
-                <Clerk.FieldError className="text-sm text-red-500 "/>
+                <Clerk.FieldError className="text-sm text-red-500 " />
               </Clerk.Field>
               <div className="space-y-5">
                 <SignUp.Action
@@ -94,48 +94,63 @@ const SignUpPage = () => {
               </div>
             </SignUp.Step>
 
-            <SignUp.Step name="verifications">
+            <SignUp.Step
+              name="verifications"
+              className="w-full flex flex-col justify-center items-start gap-6 "
+            >
               <SignUp.Strategy name="email_code">
-                <h1>Check your email</h1>
+                <h1 className="text-4xl text-black/90 font-semibold">
+                  Check your email
+                </h1>
 
-                <Clerk.Field name="code">
-                  <Clerk.Label>Email Code</Clerk.Label>
-                 <Clerk.Input
-  type="otp"
-  required
-  className="flex justify-center gap-1"
-  render={({ value, status }) => (
-    <div
-      data-status={status}
-      className="relative h-9 w-8 rounded-md bg-white ring-1 ring-inset ring-zinc-300 data-[status=selected]:bg-sky-400/10 data-[status=selected]:shadow-[0_0_8px_2px_theme(colors.sky.400/30%)] data-[status=selected]:ring-sky-400"
-    >
-      <AnimatePresence>
-        {value && (
-          <motion.span
-            initial={{ opacity: 0, scale: 0.75 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.75 }}
-            className="absolute inset-0 flex items-center justify-center text-zinc-950"
-          >
-            {value}
-          </motion.span>
-        )}
-        {value}
-      </AnimatePresence>
-      {status === 'cursor' && (
-        <motion.div
-          layoutId="otp-input-focus"
-          transition={{ ease: [0.2, 0.4, 0, 1], duration: 0.2 }}
-          className="absolute inset-0 z-10 rounded-[inherit] border border-sky-400 bg-sky-400/10 shadow-[0_0_8px_2px_theme(colors.sky.400/30%)]"
-        />
-      )}
-    </div>
-  )}
-/>
+                <Clerk.Field name="code" className=" ">
+                  <Clerk.Label className="text-[20px] text-black/90 font-semibold">
+                    Email Code
+                  </Clerk.Label>
+                  <Clerk.Input
+                    type="otp"
+                    required
+                    className="flex justify-center gap-2 pt-6"
+                    render={({ value, status }) => (
+                      <div
+                        data-status={status}
+                        className="relative h-9 w-8 rounded-md bg-white ring-1 ring-inset ring-zinc-300 data-[status=selected]:bg-sky-400/10 data-[status=selected]:shadow-[0_0_8px_2px_theme(colors.sky.400/30%)] data-[status=selected]:ring-sky-400"
+                      >
+                        <AnimatePresence>
+                          {value && (
+                            <motion.span
+                              initial={{ opacity: 0, scale: 0.75 }}
+                              animate={{ opacity: 1, scale: 1 }}
+                              exit={{ opacity: 0, scale: 0.75 }}
+                              className="absolute inset-0 flex items-center justify-center text-zinc-950"
+                            >
+                              {value}
+                            </motion.span>
+                          )}
+                          {value}
+                        </AnimatePresence>
+                        {status === "cursor" && (
+                          <motion.div
+                            layoutId="otp-input-focus"
+                            transition={{
+                              ease: [0.2, 0.4, 0, 1],
+                              duration: 0.2,
+                            }}
+                            className="absolute inset-0 z-10 rounded-[inherit] border border-sky-400 bg-sky-400/10 shadow-[0_0_8px_2px_theme(colors.sky.400/30%)]"
+                          />
+                        )}
+                      </div>
+                    )}
+                  />
                   <Clerk.FieldError />
                 </Clerk.Field>
 
-                <SignUp.Action submit>Verify</SignUp.Action>
+                <SignUp.Action
+                  submit
+                  className="w-full h-14 flex items-center justify-center text-[20px] text-white bg-[#DB4444] hover:bg-[#DB5555] cursor-pointer rounded "
+                >
+                  Verify
+                </SignUp.Action>
               </SignUp.Strategy>
             </SignUp.Step>
             {/* CAPTCHA Widget */}
