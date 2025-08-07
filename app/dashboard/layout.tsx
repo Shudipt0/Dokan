4bd47d2eecfd088c28e8f2ee6766bd3841561022
Shudipt0 <royshudipto318@gmail.com>
2025-08-07 19:55:34 +0600

    shadcn-dashboard day-1

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import AppSidebar from "./_components/AppSidebar";
 import Navbar from "./_components/Navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function DashboardLayout({
   children,
@@ -16,11 +17,13 @@ export default function DashboardLayout({
         enableSystem
         disableTransitionOnChange
       >
+        <SidebarProvider>
         <AppSidebar />
         <div className="w-full">
           <Navbar />
           <div className="px-4">{children}</div>
         </div>
+        </SidebarProvider>
       </ThemeProvider>
     </section>
   );
